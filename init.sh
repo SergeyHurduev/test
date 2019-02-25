7ebb7e0d5745d0559df52b11b85df24d0eb54668
Sergey Hurduev <svkhurdu@mts.ru>
2019-02-25 14:31:33 +0300

    Поправил init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 mkdir -p /home/box/web/uploads /home/box/web/public/img  /home/box/web/public/css /home/box/web/public/js
-=======
->>>>>>> ce3e51b4166391f8de94a023f3a36c8871b44c5b
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
