8cc758b4bdb19b8c02ded4bea6412ca2bd870344
Sergey Hurduev <svx65@mail.ru>
2019-03-07 21:06:46 +0300

    sqlite3 database added

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,10 +76,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic',
-        'USER': 'root',
-        'PASSWORD': '1234',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
