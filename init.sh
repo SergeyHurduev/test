6f79bc127adb7fe088f27c1ba1314b506b110734
Sergey Hurduev <svkhurdu@mts.ru>
2019-03-07 08:46:58 +0300

    Добавил команду старта mysql

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #gunicorn -w 4 -c --bind 127.0.0.1:8080 hello:app
+#gunicorn -w 4 -c --bind 127.0.0.1:8000 qa:application
 #sudo /etc/init.d/mysql start
 
