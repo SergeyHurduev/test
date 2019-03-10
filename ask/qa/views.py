4d0491570453f1d942fd74df4b1ddbff1b56e9b8
Sergey Hurduev <svx65@mail.ru>
2019-03-10 14:58:07 +0300

    django1 modified

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
+
+# Create your views here.
+from django.core.paginator import Paginator
+
+from .models import Question
+from .models import Answer
 
 # Create your views here.
 
@@ -6,3 +15,28 @@ from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def news(request, *args, **kwargs):
+	
+	try:
+		limit = int(request.GET.get('limit', 10))
+		if limit < 0 or limit > 100:
+			limit = 10
+	except:
+		limit = 10
+	try:
+		page = int(request.GET.get('page', 1))
+		if page < 0:
+			page = 1
+	except:
+		page = 1
+	
+	paginator = Paginator(Question.objects.new(), limit)
+	paginator.baseurl = "?page="
+	questions = paginator.page(page)
+	
+	return render( request, 'news.html', {
+		'questions': questions,
+		'paginator': paginator,
+		'page': page
+	})
